ea0a260d8fb941e6e4d8488e13fda2edbd731d25
Tim Brady <tfb414@gmail.com>
2017-07-26 16:23:40 -0400

    when buttons are clicked it searches for it's data target

--- scripts/main.js
+++ scripts/main.js
@@ -6,18 +6,19 @@ function initialize(){
     var translationsAlreadyMade = {
         house: "casa",
     };
-    (button['target']['dataset']['target'])
     
     $(document).ready(function() {
-        $('[data-target="main-panel"]').click(function(button){
-            console.log(button['target']['dataset']['target']);
+        $('[data-target="main-panel"]').find($("button")).click(function(button){
+            var searchData = dataToTranslate(button['target']['dataset']['target']);
+            // console.log(button['target']['dataset']['target']);
+            console.log(translationsAlreadyMade);
+            makeRequestToTranslator(searchData, translationsAlreadyMade);
         })
     })
 
 }
 
-function dataToTranslate(){
-    var searchString = "house"
+function dataToTranslate(searchString){
     var targetLanguage = "es"
     var data = {
         "key": "AIzaSyCE7H2z-RTJBWk12jV2JIJTnU83ryAJD3Q",
@@ -34,10 +35,10 @@ function makeRequestToTranslator(data, translationsAlreadyMade){
     if (translationsAlreadyMade[data['q']]){
         return translationsAlreadyMade[data['q']];
     }
-    // $.post("https://translation.googleapis.com/language/translate/v2", data, printIt)
-    //     .then(function(d){
-    //         translationsAlreadyMade[data['q']] = d['data']['translations']['0']['translatedText'];
-    //     });
+    $.post("https://translation.googleapis.com/language/translate/v2", data, printIt)
+        .then(function(d){
+            translationsAlreadyMade[data['q']] = d['data']['translations']['0']['translatedText'];
+        });
     //I have this commented out so that it does not run anytime you refresh
 }
 
