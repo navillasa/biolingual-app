db756e711c8b290f9ed584d9c5ce9bf0741ae2e2
Tim Brady <tfb414@gmail.com>
2017-07-26 15:06:29 -0400

    caches requests to the server to see if they have requested it already

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,18 @@
 
 
 function initialize(){
-    console.log('derp')
     const apiKey = "AIzaSyCE7H2z-RTJBWk12jV2JIJTnU83ryAJD3Q";
     var data = dataToTranslate();
-    var translationAlreadyMade = {};
-    makeRequestToTranslator(data).then();
-    console.log(derp);
+    var translationsAlreadyMade = {
+        house: "casa",
+    };
+    makeRequestToTranslator(data, translationsAlreadyMade);
+    console.log(translationsAlreadyMade);
 
 }
 
 function dataToTranslate(){
-    var searchString = "house"
+    var searchString = "mountains"
     var targetLanguage = "es"
     var data = {
         "key": "AIzaSyCE7H2z-RTJBWk12jV2JIJTnU83ryAJD3Q",
@@ -23,8 +24,15 @@ function dataToTranslate(){
 //I am building this out like this because later we will be pulling the searchstring, the language and possibly other things. 
 //We can replace Searchstring/targetLanguage with another function to grab the text.
 
-function makeRequestToTranslator(data){
-    return $.post("https://translation.googleapis.com/language/translate/v2", data, printIt);
+function makeRequestToTranslator(data, translationsAlreadyMade){
+    // console.log(translationsAlreadyMade[data['q']]);
+    if (translationsAlreadyMade[data['q']]){
+        return translationsAlreadyMade[data['q']];
+    }
+    $.post("https://translation.googleapis.com/language/translate/v2", data, printIt)
+        .then(function(d){
+            translationsAlreadyMade[data['q']] = d['data']['translations']['0']['translatedText'];
+        });
 }
 
 function printIt(text){
