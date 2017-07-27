0a78dbd0ef59c9716e70769e8f8404e28c4f4c84
Tim Brady <tfb414@gmail.com>
2017-07-27 09:16:26 -0400

    begins adding language options

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,17 @@
 
 
 function initialize(){
-    const apiKey = "AIzaSyCE7H2z-RTJBWk12jV2JIJTnU83ryAJD3Q";
-    var data = dataToTranslate();
+    // var data = dataToTranslate();
     var translationsAlreadyMade = {
         house: "casa",
     };
     
     $(document).ready(function() {
-        $('[data-target="main-panel"]').find($("button")).click(function(button){
+        $('[data-target="main-panel"] button').click(function(button){
             var searchData = dataToTranslate(button['target']['dataset']['target']);
             // console.log(button['target']['dataset']['target']);
-            console.log(translationsAlreadyMade);
-            makeRequestToTranslator(searchData, translationsAlreadyMade);
+            // console.log(translationsAlreadyMade);
+            retrieveTranslation(searchData, translationsAlreadyMade);
         })
     })
 
@@ -30,15 +29,16 @@ function dataToTranslate(searchString){
 //I am building this out like this because later we will be pulling the searchstring, the language and possibly other things. 
 //We can replace Searchstring/targetLanguage with another function to grab the text.
 
-function makeRequestToTranslator(data, translationsAlreadyMade){
+function retrieveTranslation(data, translationsAlreadyMade){
     // console.log(translationsAlreadyMade[data['q']]);
     if (translationsAlreadyMade[data['q']]){
         return translationsAlreadyMade[data['q']];
     }
-    // $.post("https://translation.googleapis.com/language/translate/v2", data, printIt)
-    //     .then(function(d){
-    //         translationsAlreadyMade[data['q']] = d['data']['translations']['0']['translatedText'];
-    //     });
+    $.post("https://translation.googleapis.com/language/translate/v2", data, printIt)
+        .then(function(d){
+            console.log(translationsAlreadyMade[data['q']] = d['data']['translations']['0']['translatedText']);
+            translationsAlreadyMade[data['q']] = d['data']['translations']['0']['translatedText'];
+        });
     //I have this commented out so that it does not run anytime you refresh
 }
 
