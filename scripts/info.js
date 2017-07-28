3c669b66ab5d4ea2e226ca74bdddbfe4f642bae4
jenlij <jljohns1216@gmail.com>
2017-07-28 14:26:27 -0400

    creates info.js to test menu selectors

--- scripts/info.js
+++ scripts/info.js
@@ -1,14 +1,23 @@
-function turnOnInfo() {
+function turnOn() {
     console.log("on");
 }
 
-function turnOffInfo() {
+function turnOff() {
     console.log('off');
 }
 
+function selectLang() {
+   console.log($('[data-target=select]').val());
+}
 
-$('[data-target=info-on]').on('click', turnOnInfo);
-$('[data-target=info-off]').on('click', turnOffInfo);
 
 
 
+$('[data-target=info-on]').on('click', turnOn);
+$('[data-target=info-off]').on('click', turnOff);
+
+$('[data-target=sym-on]').on('click', turnOn);
+$('[data-target=sym-off]').on('click', turnOff);
+
+
+$('[data-target=select]').on('change', selectLang);
