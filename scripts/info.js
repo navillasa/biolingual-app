756cc2c73bcadb1fc32d7a023fcb0e8c5659ea9b
jenlij <jljohns1216@gmail.com>
2017-07-28 15:14:54 -0400

    adds wiki link

--- scripts/info.js
+++ scripts/info.js
@@ -10,10 +10,15 @@ function selectLang() {
    console.log($('[data-target=select]').val());
 }
 
+function goToWiki(searchTerm) {
+    window.open('https://en.wikipedia.org/wiki/' + searchTerm);
+}
 
 
+$('[data-target=info-on]').on('click', function() {
+    goToWiki('Hand');
+});
 
-$('[data-target=info-on]').on('click', turnOn);
 $('[data-target=info-off]').on('click', turnOff);
 
 $('[data-target=sym-on]').on('click', turnOn);
