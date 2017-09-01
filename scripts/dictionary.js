e97f82acce578a7f194602813e240370b7525c9c
jenlij <jljohns1216@gmail.com>
2017-09-01 13:55:49 -0400

    demo dictionary

---
+++ scripts/dictionary.js
@@ -0,0 +1,499 @@
+var english = {
+    "Head": [ 
+        "Headache",
+        "Fever",
+        "Tiredness",
+        "Nausea",
+        "Sleeplessness",
+        "Weakness",
+        "Nervousness",
+        "Unconsciousness",
+        "Dizziness",
+        "Memory loss"
+    ],
+
+    "Face": [
+        "Eye redness",
+        "Itchy eyes",
+        "Burning eyes",
+        "Feeling of foreign body in the eye",
+        "Cheek swelling",
+        "Tears",
+        "Drooping eyelid",
+        "Dry eyes",
+        "Eye pain"   
+    ],
+
+    "Ear, Nose and Throat": [
+        "Sore throat",
+        "Runny nose",
+        "Cough",
+        "Stuffy nose",
+        "Wheezing",
+        "Thirst",
+        "Throat irritation",
+        "Earache",
+        "Sneezing",
+        "Nose itching",
+        "Hiccups",
+        "Neck pain",
+        "Swollen glands",
+        "Painful swallowing"
+    ],
+
+    "Mouth": [
+        "Cough",
+        "Lip swelling",
+        "Thirst",
+        "Reduced appetite",
+        "Sputum",
+        "Vomiting",
+        "Hiccups",
+        "Fast breathing",
+        "Vomiting blood",
+        "Painful swallowing",
+        "Sores",
+        "Toothache",
+        "Tongue swelling"
+    ],
+
+    "Chest": [
+        "Cough",
+        "Chest pain",
+        "Shortness of breath",
+        "Wheezing",
+        "Chest tightness",
+        "Palpitations",
+        "Nausea",
+        "Heartburn",
+        "Sputum"            
+    ],
+
+    "Arm": [
+        "Pain",
+        "Paralysis",
+        "Swollen glands in the armpits",
+        "Soreness",
+        "Elbow stiffness",
+        "Bruising"
+    ],
+
+    "Hand and Wrist": [
+        "Tremor at rest",
+        "Stiffness",
+        "Aching",
+        "Swelling",
+        "Numbness"
+    ],
+
+    "Abdomen": [
+        "Abdominal pain",
+        "Nausea",
+        "Heartburn",
+        "Reduced appetite",
+        "Vomiting",
+        "Trouble breathing",
+        "Stomach pain",
+        "Back pain",
+        "Bloating"
+        
+    ],
+
+    "Groin": [
+        "Menstrual pain",
+        "Missed period",
+        "Pain",
+        "Itchiness",
+        "Burning"
+    ],
+
+    "Leg": [
+        "Pain",
+        "Soreness",
+        "Stiffness",
+        "Paralysis",
+        "Swelling"
+    ],
+    "Foot": [
+        "Tremor",
+        "Stiffness",
+        "Aching",
+        "Swollen",
+        "Numbness"
+    ]
+};
+
+var mandarin = {
+    "头": [
+        "头痛",
+        "发热",
+        "疲劳",
+        "恶心",
+        "失眠",
+        "弱点",
+        "精神紧张",
+        "神志不清",
+        "头晕",
+        "记忆丧失"
+    ],
+    
+    "面对": [
+        "眼睛发红",
+        "眼睛痒",
+        "燃烧的眼睛",
+        "感觉异物在眼中",
+        "脸颊肿胀",
+        "眼泪",
+        "下垂眼睑",
+        "干眼睛",
+        "眼睛疼痛"
+    ],
+
+    "耳鼻喉咙": [
+        "喉咙痛",
+        "流鼻涕",
+        "咳嗽",
+        "臭鼻子",
+        "喘息",
+        "口渴",
+        "喉咙刺激",
+        "耳痛",
+        "打喷嚏",
+        "鼻痒",
+        "打嗝",
+        "颈部疼痛",
+        "肿胀腺体",
+        "痛苦的吞咽"
+    ],
+
+    "口": [
+        "咳嗽",
+        "唇肿胀",
+        "口渴",
+        "食欲降低",
+        "痰",
+        "呕吐",
+        "打嗝",
+        "呼吸快",
+        "呕血",
+        "痛苦的吞咽",
+        "疮",
+        "牙疼",
+        "舌肿胀"
+    ],
+
+    "胸部": [
+        "咳嗽",
+        "胸痛",
+        "呼吸急促",
+        "喘息",
+        "胸部紧迫感",
+        "心悸",
+        "恶心",
+        "胃灼热",
+        "痰"
+    ],
+
+    "臂": [
+        "疼痛",
+        "麻痹",
+        "腋窝肿胀腺体",
+        "疼痛",
+        "弯头刚度",
+        "瘀血"
+    ],
+
+    "手和手腕": [
+        "休息时震颤",
+        "刚性",
+        "疼痛",
+        "肿",
+        "麻木"
+    ],
+
+    "腹部": [
+        "腹痛",
+        "恶心",
+        "胃灼热",
+        "食欲减退",
+        "呕吐",
+        "呼吸困难",
+        "肚子疼",
+        "背疼",
+        "腹胀"
+    ],
+
+    "腹股沟": [
+        "月经痛",
+        "错过的时期",
+        "疼痛",
+        "痕痒",
+        "燃烧"
+    ],
+
+    "腿": [
+        "疼痛",
+        "疼痛",
+        "刚性",
+        "麻痹",
+        "肿胀"
+        
+    ],
+    "脚丫子": [
+        "震颤",
+        "刚性",
+        "疼痛",
+        "肿",
+        "麻木",
+    ]
+};
+
+var spanish = {
+    "Cabeza": [
+        "Dolor de cabeza",
+        "Fiebre",
+        "Cansancio",
+        "Náusea",
+        "Insomnio",
+        "Debilidad",
+        "Nerviosísmo",
+        "Inconsciencia",
+        "Mareo",
+        "Pérdida de memoria"
+    ],
+
+    "Cara": [
+        "Enrojecimiento de los ojos",
+        "Ojos que pican",
+        "Ojos irritados",
+        "Sensación de cuerpo extraño en el ojo",
+        "Inflamación de la mejilla",
+        "Lágrimas",
+        "El párpado caído",
+        "Ojos secos",
+        "Dolor de ojo"
+    ],
+
+    "Oído, nariz y garganta": [
+        "Dolor de garganta",
+        "Nariz que moquea",
+        "Tos",
+        "Congestión nasal",
+        "Sibilancias",
+        "Sed",
+        "Irritación de garganta",
+        "Dolor de oidos",
+        "Estornudos",
+        "Picazón en la nariz",
+        "Hipo",
+        "Dolor de cuello",
+        "Glándulas hinchadas",
+        "Dolorosa deglución"
+    ],
+    
+    "Boca": [
+        "Tos",
+        "Hinchazón de los labios",
+        "Sed",
+        "Reducción del apetito",
+        "Esputo",
+        "Vómitos",
+        "Hipo",
+        "Respiración rápida",
+        "Vómitos de sangre",
+        "Dolor de la deglución",
+        "Llagas",
+        "Dolor de muelas",
+        "Hinchazón de la lengua"
+    ],
+
+    "Chest": [
+        "Tos",
+        "Dolor de pecho",
+        "Dificultad para respirar",
+        "Sibilancias",
+        "Opresión en el pecho",
+        "Palpitaciones",
+        "Náusea",
+        "Acidez",
+        "Esputo"
+    ],
+
+    "Brazo": [
+        "Dolor",
+        "Parálisis",
+        "Glándulas hinchadas en las axilas",
+        "Inflamación",
+        "Rigidez del codo",
+        "Moretones"
+    ],
+
+    "Mano y muñeca": [
+        "Temblor en reposo",
+        "Rigidez",
+        "Dolor",
+        "Hinchado",
+        "Entumecimiento"
+    ],
+
+    "Abdomen": [
+        "Dolor abdominal",
+        "Náusea",
+        "Acidez",
+        "Reducción del apetito",
+        "Vómitos",
+        "Dificultad para respirar",
+        "Dolor de estómago",
+        "Dolor de espalda",
+        "Hinchazón"
+    ],
+
+    "Ingle": [
+        "Dolor menstrual",
+        "Período perdido",
+        "Dolor",
+        "Picazón",
+        "Ardiente"
+    ],
+
+    "Pierna": [
+        "Dolor",
+        "Dolor",
+        "Rigidez",
+        "Parálisis",
+        "Hinchazón"
+    ],
+
+    "Pie": [
+        "Temblor",
+        "Rigidez",
+        "Dolor",
+        "Hinchado",
+        "Entumecimiento"
+    ]
+};
+
+var french = {
+    "Tête": [
+        "Mal de tête",
+        "Fièvre",
+        "Fatigue",
+        "La nausée",
+        "Insomnie",
+        "La faiblesse",
+        "Nervosité",
+        "Inconscience",
+        "Vertiges",
+        "Perte de mémoire"
+    ],
+
+    "Visage": [
+        "Rougeur des yeux",
+        "Yeux qui grattent",
+        "Les yeux brûlants",
+        "Sensation de corps étranger dans les yeux",
+        "L'enflure des joues",
+        "Larmes",
+        "Paupière tombante",
+        "Yeux secs",
+        "Douleur oculaire"
+    ],
+
+    "Nez, gorge et l'oreille": [
+        "Gorge irritée",
+        "Nez qui coule",
+        "La toux",
+        "Nez encombré",
+        "Sifflements",
+        "La soif",
+        "Irritation de la gorge",
+        "Mal d'oreille",
+        "Éternuements",
+        "Nez démangeaisons",
+        "Le hoquet",
+        "La douleur du cou",
+        "Glandes enflées",
+        "Déglutition douloureuse"
+    ],
+
+    "Bouche": [
+        "La toux",
+        "Gonflement des lèvres",
+        "La soif",
+        "Réduction de l'appétit",
+        "Expectorations",
+        "Vomissement",
+        "Le hoquet",
+        "Respiration rapide",
+        "Vomir le sang",
+        "Ingestion douloureuse",
+        "Les plaies",
+        "Mal aux dents",
+        "Gonflement de la langue"
+    ],
+
+    "Poitrine": [
+        "La toux",
+        "Pain à la poitrine",
+        "Essoufflement",
+        "Respiration sifflante",
+        "Oppression thoracique",
+        "Palpitations",
+        "La nausée",
+        "Brûlures d'estomac",
+        "Expectorations"
+    ],
+
+    "Bras": [
+        "Douleur",
+        "Paralysie",
+        "Glandes gonflées dans les aisselles",
+        "Douleur",
+        "Rigidité du coude",
+        "Contusion"
+    ],
+
+    "Main et poignet": [
+        "Tremblements au repos",
+        "Rigidité",
+        "Douloureux",
+        "Gonflé",
+        "Engourdissement"
+    ],
+
+    "Abdomen": [
+        "Douleur abdominale",
+        "La nausée",
+        "Brûlures d'estomac",
+        "Réduction de l'appétit",
+        "Vomissement",
+        "Difficulté à respirer",
+        "Douleur d'estomac",
+        "Mal au dos",
+        "Ballonnements"
+    ],
+
+    "Aine": [
+        "Les douleurs menstruelles",
+        "Période manquée",
+        "Douleur",
+        "Démangeaisons",
+        "Brûlant"
+    ],
+
+    "Jambe": [
+        "Douleur",
+        "Douleur",
+        "Rigidité",
+        "Paralysie",
+        "Gonflement"
+    ],
+
+    "Pied": [
+        "Tremblement",
+        "Rigidité",
+        "Douloureux",
+        "Gonflé",
+        "Engourdissement"
+    ]
+};
