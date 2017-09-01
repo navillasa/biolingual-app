dd83310732934f471b2eecdb667fab9244105634
jenlij <jljohns1216@gmail.com>
2017-09-01 11:54:45 -0400

    adding translations

---
+++ scripts/dictionary.js
@@ -0,0 +1,53 @@
+var biodict = {
+    "en":{
+        "Head": [ 
+            "Headache",
+            "Fever",
+            "Tiredness",
+            "Nausea",
+            "Sleeplessness",
+            "Weakness",
+            "Nervousness",
+            "Unconsciousness",
+            "Dizziness",
+            "Memory loss"
+            ],
+
+        "Face": [
+            "Eye redness",
+            "Itchy eyes",
+            "Burning eyes",
+            "Feeling of foreign body in the eye",
+            "Cheek swelling",
+            "Tears",
+            "Drooping eyelid",
+            "Dry eyes",
+            "Eye pain"   
+            ],
+            
+        "Ear, Nose and Throat": [],
+        "Mouth": [],
+        "Chest": [],
+        "Arm": [],
+        "Hand and Wrist": [],
+        "Abdomen": [],
+        "Groin": [],
+        "Leg": [],
+        "Foot": []
+    },
+    "es": {},
+    "zh-CN": {
+        "头": [],
+        "面对": [],
+        "耳鼻喉咙": [],
+        "口": [],
+        "胸部": [],
+        "臂": [],
+        "手和手腕": [],
+        "腹部": [],
+        "腹股沟": [],
+        "腿": [],
+        "脚丫子": []
+    },
+    'fr': {}
+}
